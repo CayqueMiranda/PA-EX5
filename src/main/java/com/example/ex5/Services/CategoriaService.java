72588450008cd096b9e271e5347e0af46e2d41b5
CayqueMiranda <cayquemiranda2020@gmail.com>
2026-03-22 14:30:32 -0300

    Service adicionado

---
+++ src/main/java/com/example/ex5/Services/CategoriaService.java
@@ -0,0 +1,37 @@
+package com.example.ex5.Services;
+
+import com.example.ex5.Models.CategoriaModel;
+import com.example.ex5.Repositories.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+
+    private CategoriaRepository categoriaRepository;
+
+    public CategoriaRepository getCategoriaRepository() {
+        return categoriaRepository;
+    }
+
+    public void setCategoriaRepository(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public CategoriaModel criar(CategoriaModel categoriaModel) {
+        return categoriaRepository.save(categoriaModel);
+    }
+
+    public List<CategoriaModel> listar() {
+        return categoriaRepository.findAll();
+    }
+
+    public CategoriaModel buscarPorId(Long id) {
+        return categoriaRepository.findById(id).get();
+    }
+
+    public void deletar(CategoriaModel categoriaModel) {
+        categoriaRepository.delete(categoriaModel);
+    }
+}
